3b04e63efe59abe8108ff1892dc621c8317db289
Srivatsaugosavi <srivatsagosavi@gmail.com>
2021-11-02 16:43:38 +0530

    basic_nodejs

---
+++ datatype.js
@@ -0,0 +1,21 @@
+var num = 20; 
+console.log("type of variable is : ",typeof(num));
+
+var str = 'helloo'; 
+console.log("type of variable is : ",typeof(str));
+
+var boo = true; 
+console.log("type of variable is : ",typeof(boo));
+
+var x;
+console.log(x);
+
+var a = null;
+console.log("type of a variable is : ",typeof(a));
+
+var b = 8.5;
+console.log("type of b variable is : ",typeof(b));
+
+var c = -555;
+console.log("type of c variable is : ",typeof(c));
+
